579972609609e854c046cbb6256322e2e324d3a5
GitSmall <wangjgtao@126.com>
2023-01-28 08:59:01 +0000

    Deploying to gh-pages from @ GitSmall/vite-vue3-js-template@5e3aea29a4126d6ab5dba1dd777fe6dba61766d0 🚀

---
+++ assets/AboutView-7e9117e5.js
@@ -0,0 +1,1 @@
+import{_ as r,r as n,o as c,c as s,a as t,w as o,F as _,b as a}from"./index-3227fd94.js";const l={};function u(f,m){const e=n("router-link");return c(),s(_,null,[t(e,{to:"/"},{default:o(()=>[a("Home")]),_:1}),t(e,{to:"/about"},{default:o(()=>[a("About")]),_:1})],64)}const i=r(l,[["render",u]]);export{i as default};
